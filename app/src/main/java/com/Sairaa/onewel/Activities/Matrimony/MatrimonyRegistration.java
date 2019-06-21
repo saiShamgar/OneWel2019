7009a5beb05ae9f27318c400bf0ebebb9cf688e1
unknown <sainookaraju431@gmail.com>
2019-06-21 17:35:14 +0530

    working on registration page

--- app/src/main/java/com/Sairaa/onewel/Activities/Matrimony/MatrimonyRegistration.java
+++ app/src/main/java/com/Sairaa/onewel/Activities/Matrimony/MatrimonyRegistration.java
@@ -1,24 +1,33 @@
 package com.Sairaa.onewel.Activities.Matrimony;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.EditText;
-import android.widget.RadioGroup;
-import android.widget.Spinner;
+import android.util.Log;
+import android.view.View;
+import android.widget.*;
+import com.Sairaa.onewel.BaseActivity;
 import com.Sairaa.onewel.R;
 
-public class MatrimonyRegistration extends AppCompatActivity {
+import java.util.ArrayList;
 
-    private EditText edt_mat_reg_name,edt_mat_reg_surname,edt_mat_reg_phone_num;
+public class MatrimonyRegistration extends BaseActivity  {
+
+    private EditText edt_mat_reg_name,edt_mat_reg_surname,edt_mat_reg_phone_num,edt_mat_reg_city,edt_mat_reg_gothra;
     private RadioGroup edt_mat_reg_gender,edt_mat_reg_marital_status,edt_mat_reg_physical_status,edt_mat_reg_eating,edt_mat_reg_drinking,edt_mat_reg_smoking;
     private Spinner edt_mat_reg_age,edt_mat_reg_height_in_feet,edt_mat_reg_heightin_inch,edt_mat_reg_religion,edt_mat_reg_mother_tongue,
              edt_mat_reg_caste_devision,edt_mat_reg_highest_study,edt_mat_reg_occupation,edt_mat_reg_annual_income,edt_mat_reg_country
-            ,edt_mat_reg_state,edt_mat_reg_district,edt_mat_reg_city,edt_mat_reg_rashi,edt_mat_reg_star,edt_mat_reg_gothra;
+            ,edt_mat_reg_state,edt_mat_reg_district,edt_mat_reg_rashi,edt_mat_reg_star;
+
+    private Context context;
+    private String religion,motherTongue,state;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_matrimony_registration);
+
+        context=MatrimonyRegistration.this;
 
         edt_mat_reg_phone_num=findViewById(R.id.edt_mat_reg_phone_num);
         edt_mat_reg_surname=findViewById(R.id.edt_mat_reg_surname);
@@ -46,7 +55,231 @@ public class MatrimonyRegistration extends AppCompatActivity {
         edt_mat_reg_star=findViewById(R.id.edt_mat_reg_star);
         edt_mat_reg_gothra=findViewById(R.id.edt_mat_reg_gothra);
 
+        SetAdapter(edt_mat_reg_religion,getResources().getStringArray(R.array.religion_array));
+        SetAdapter(edt_mat_reg_mother_tongue,getResources().getStringArray(R.array.mother_tongue_hindu));
+        SetAdapter(edt_mat_reg_country,getResources().getStringArray(R.array.countries));
+        SetAdapter(edt_mat_reg_state,getResources().getStringArray(R.array.india_states));
+        SetAdapter(edt_mat_reg_rashi,getResources().getStringArray(R.array.Rashi));
+        SetAdapter(edt_mat_reg_star,getResources().getStringArray(R.array.Nakshatras));
+
+        edt_mat_reg_state.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+               state=parent.getSelectedItem().toString();
+               if (state.contains("Andhra Pradesh")){
+                   SetAdapter(edt_mat_reg_district,getResources().getStringArray(R.array.AndhraPradesh));
+               }
+               else if (state.contains("Arunachal Pradesh")){
+                   SetAdapter(edt_mat_reg_district,getResources().getStringArray(R.array.ArunachalPradesh));
+               }
+               else if (state.contains("Assam")){
+                   SetAdapter(edt_mat_reg_district,getResources().getStringArray(R.array.Assam));
+               }
+               else if (state.contains("Bihar")){
+                   SetAdapter(edt_mat_reg_district,getResources().getStringArray(R.array.Bihar));
+               }
+               else if (state.contains("Chandigarh")){
+                   SetAdapter(edt_mat_reg_district,getResources().getStringArray(R.array.Chandigarh));
+               }
+               else if (state.contains("Chhattisgarh")){
+                   SetAdapter(edt_mat_reg_district,getResources().getStringArray(R.array.Chhattisgarh));
+               }
+               else if (state.contains("Delhi")){
+                   SetAdapter(edt_mat_reg_district,getResources().getStringArray(R.array.Delhi));
+               }
+               else if (state.contains("Andaman and Nicobar Islands")){
+                   SetAdapter(edt_mat_reg_district,getResources().getStringArray(R.array.Andaman_Nicobar));
+               }
+               else if (state.contains("Dadra and Nagar Haveli")){
+                   SetAdapter(edt_mat_reg_district,getResources().getStringArray(R.array.Dadra_Nagar_Haveli));
+               }
+               else if (state.contains("Daman and Diu")){
+                   SetAdapter(edt_mat_reg_district,getResources().getStringArray(R.array.Daman_Diu));
+               }
+               else if (state.contains("Goa")){
+                   SetAdapter(edt_mat_reg_district,getResources().getStringArray(R.array.Goa));
+               }
+               else if (state.contains("Gujarat")){
+                   SetAdapter(edt_mat_reg_district,getResources().getStringArray(R.array.Gujarat));
+               }
+               else if (state.contains("Haryana")){
+                   SetAdapter(edt_mat_reg_district,getResources().getStringArray(R.array.Haryana));
+               }
+               else if (state.contains("Himachal Pradesh")){
+                   SetAdapter(edt_mat_reg_district,getResources().getStringArray(R.array.Himachal_Pradesh));
+               }
+               else if (state.contains("Jammu and Kashmir")){
+                   SetAdapter(edt_mat_reg_district,getResources().getStringArray(R.array.Jammu_Kashmir));
+               }
+               else if (state.contains("Jharkhand")){
+                   SetAdapter(edt_mat_reg_district,getResources().getStringArray(R.array.Jharkhand));
+               }
+               else if (state.contains("Karnataka")){
+                   SetAdapter(edt_mat_reg_district,getResources().getStringArray(R.array.Karnataka));
+               }
+               else if (state.contains("Kerala")){
+                   SetAdapter(edt_mat_reg_district,getResources().getStringArray(R.array.Kerala));
+               }
+               else if (state.contains("Lakshadweep")){
+                   SetAdapter(edt_mat_reg_district,getResources().getStringArray(R.array.Lakshadweep));
+               }
+               else if (state.contains("Madhya Pradesh")){
+                   SetAdapter(edt_mat_reg_district,getResources().getStringArray(R.array.Madhya_Pradesh));
+               }
+               else if (state.contains("Maharashtra")){
+                   SetAdapter(edt_mat_reg_district,getResources().getStringArray(R.array.Maharashtra));
+               }
+               else if (state.contains("Manipur")){
+                   SetAdapter(edt_mat_reg_district,getResources().getStringArray(R.array.Manipur));
+               }
+               else if (state.contains("Meghalaya")){
+                   SetAdapter(edt_mat_reg_district,getResources().getStringArray(R.array.Meghalaya));
+               }
+               else if (state.contains("Mizoram")){
+                   SetAdapter(edt_mat_reg_district,getResources().getStringArray(R.array.Mizoram));
+               }
+               else if (state.contains("Nagaland")){
+                   SetAdapter(edt_mat_reg_district,getResources().getStringArray(R.array.Nagaland));
+               }
+               else if (state.contains("Orissa")){
+                   SetAdapter(edt_mat_reg_district,getResources().getStringArray(R.array.Odisha));
+               }
+               else if (state.contains("Pondicherry")){
+                   SetAdapter(edt_mat_reg_district,getResources().getStringArray(R.array.Puducherry));
+               }
+               else if (state.contains("Punjab")){
+                   SetAdapter(edt_mat_reg_district,getResources().getStringArray(R.array.Punjab));
+               }
+               else if (state.contains("Rajasthan")){
+                   SetAdapter(edt_mat_reg_district,getResources().getStringArray(R.array.Rajasthan));
+               }
+               else if (state.contains("Sikkim")){
+                   SetAdapter(edt_mat_reg_district,getResources().getStringArray(R.array.Sikkim));
+               }
+               else if (state.contains("Tamil Nadu")){
+                   SetAdapter(edt_mat_reg_district,getResources().getStringArray(R.array.Tamil_Nadu));
+               }
+               else if (state.contains("Telangana")){
+                   SetAdapter(edt_mat_reg_district,getResources().getStringArray(R.array.Telangana));
+               }
+               else if (state.contains("Tripura")){
+                   SetAdapter(edt_mat_reg_district,getResources().getStringArray(R.array.Tripura));
+               }
+               else if (state.contains("Uttaranchal")){
+                   SetAdapter(edt_mat_reg_district,getResources().getStringArray(R.array.Uttarakhand));
+               }
+               else if (state.contains("Uttar Pradesh")){
+                   SetAdapter(edt_mat_reg_district,getResources().getStringArray(R.array.Uttar_Pradesh));
+               }
+               else if (state.contains("West Bengal")){
+                   SetAdapter(edt_mat_reg_district,getResources().getStringArray(R.array.West_Bengal));
+               }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
 
 
+        edt_mat_reg_religion.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                religion=parent.getSelectedItem().toString();
+                if (religion.contains("Musilm-shia")){
+                    SetAdapter(edt_mat_reg_caste_devision,getResources().getStringArray(R.array.musilim_castes));
+                }
+                else if (religion.contains("Musilm-sunni")){
+                    SetAdapter(edt_mat_reg_caste_devision,getResources().getStringArray(R.array.musilim_castes));
+                }
+                else if (religion.contains("Musilm-others")){
+                    SetAdapter(edt_mat_reg_caste_devision,getResources().getStringArray(R.array.musilim_castes));
+                }
+                else if (religion.contains("Christian - Catholic")){
+                    SetAdapter(edt_mat_reg_caste_devision,getResources().getStringArray(R.array.christian_castes));
+                }
+                else if (religion.contains("Christian - Orthodox")){
+                    SetAdapter(edt_mat_reg_caste_devision,getResources().getStringArray(R.array.christian_castes));
+                }
+                else if (religion.contains("Christian - Protestant")){
+                    SetAdapter(edt_mat_reg_caste_devision,getResources().getStringArray(R.array.christian_castes));
+                }
+                else if (religion.contains("Christian - Others")){
+                    SetAdapter(edt_mat_reg_caste_devision,getResources().getStringArray(R.array.christian_castes));
+                }
+                else if (religion.contains("Sikh")){
+                    SetAdapter(edt_mat_reg_caste_devision,getResources().getStringArray(R.array.sikh_caste));
+                }
+                else if (religion.contains("Jain - Digambar")){
+                    SetAdapter(edt_mat_reg_caste_devision,getResources().getStringArray(R.array.jain_castes));
+                }
+                else if (religion.contains("Jain - Shwetambar")){
+                    SetAdapter(edt_mat_reg_caste_devision,getResources().getStringArray(R.array.jain_castes));
+                }
+                else if (religion.contains("Jain - Others")){
+                    SetAdapter(edt_mat_reg_caste_devision,getResources().getStringArray(R.array.jain_castes));
+                }
+                else if (religion.contains("Parsi")){
+                    SetAdapter(edt_mat_reg_caste_devision,getResources().getStringArray(R.array.parsi_castes));
+                }
+                else if (religion.contains("Buddhist")){
+                    SetAdapter(edt_mat_reg_caste_devision,getResources().getStringArray(R.array.buddhist_castes));
+                }
+                else if (religion.contains("No Religious Belief")){
+                    SetAdapter(edt_mat_reg_caste_devision,getResources().getStringArray(R.array.buddhist_castes));
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+        edt_mat_reg_mother_tongue.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                motherTongue=parent.getSelectedItem().toString();
+                if (religion.contains("Hindu") && motherTongue.contains("Telugu")){
+                    SetAdapter(edt_mat_reg_caste_devision,getResources().getStringArray(R.array.telugu_hindu_castes));
+                }
+                else if (religion.contains("Hindu") && motherTongue.contains("Hindi") ){
+                    SetAdapter(edt_mat_reg_caste_devision,getResources().getStringArray(R.array.hindi_hindu_castes));
+                }
+                else if (religion.contains("Hindu") && motherTongue.contains("Kannada") ){
+                    SetAdapter(edt_mat_reg_caste_devision,getResources().getStringArray(R.array.kannada_castes));
+                }
+                else if (religion.contains("Hindu") && motherTongue.contains("Tamil") ){
+                    SetAdapter(edt_mat_reg_caste_devision,getResources().getStringArray(R.array.tamil_castes));
+                }
+                else if (religion.contains("Hindu") && motherTongue.contains("Urdu") ){
+                    SetAdapter(edt_mat_reg_caste_devision,getResources().getStringArray(R.array.hindi_hindu_castes));
+                }
+                else if (religion.contains("Hindu") && motherTongue.contains("Bengali") ){
+                    SetAdapter(edt_mat_reg_caste_devision,getResources().getStringArray(R.array.bengali_castes));
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        //radioGroupSelectedText(edt_mat_reg_gender);
+    }
+    private String radioGroupSelectedText(RadioGroup id){
+        int radioButtonID = id.getCheckedRadioButtonId();
+        RadioButton radioButton = (RadioButton) id.findViewById(radioButtonID);
+        String selectedtext = (String) radioButton.getText();
+        Log.e("selectedtext",selectedtext);
+
+        return selectedtext;
+    }
+
+
+    private void SetAdapter(Spinner edt_mat_reg_caste_devision, String[] stringArray){
+       ArrayAdapter countAdapter = new ArrayAdapter<String>(context, R.layout.show_count, stringArray);
+        edt_mat_reg_caste_devision.setAdapter(countAdapter);
     }
 }
